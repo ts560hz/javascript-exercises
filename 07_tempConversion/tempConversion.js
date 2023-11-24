231e94fa5344e17a2bdbec2c8e340d2b630d6e09
unknown <timotej.suc@R3KT.metro.local>
2023-11-24 21:43:20 +0100

    Finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+// Formula is (x-32) * (5/9)
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5/9);
+  let roundedCelsius = Math.round(celsius * 10) / 10;
+  return roundedCelsius;
 };
-
-const convertToFahrenheit = function() {
+// Formula is x * (9/5) + 32
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius * (9/5) + 32;
+  let roundedFahrenheit = Math.round(fahrenheit * 10) / 10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
