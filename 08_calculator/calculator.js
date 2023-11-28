58745bddcf8d1f04876d6a927e65fde42b5d8867
ts560hz <56908908+ts560hz@users.noreply.github.com>
2023-11-28 22:01:07 +0100

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
+};
+const sum = function (array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  let totalSum = 0;
+  for (let i = 0; i < array.length; i++) {
+    totalSum += array[i];
+  }
+  return totalSum;
 };
 
-const sum = function() {
-	
+const multiply = function (array) {
+  if (array.length === 0) return 0;
+  let result = 1;
+
+  for (let i = 0; i < array.length; i++) {
+    result *= array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const power = function (a, x) {
+  let result = 1;
+  for (let i = 0; i < x; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (x) {
+  let result = 1;
+  for (let i = x; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +49,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
