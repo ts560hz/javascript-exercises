75146f2206a36096884df25768285691ac471251
ts560hz <56908908+ts560hz@users.noreply.github.com>
2023-11-28 22:09:47 +0100

    Done palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let re = /[\W_]/g;
+  let lowerStr = str.toLowerCase().replace(re, "");
 
+  let reverseStr = lowerStr.split("").reverse().join("");
+
+  return reverseStr === lowerStr;
 };
 
 // Do not edit below this line
