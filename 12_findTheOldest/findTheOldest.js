96c7fde452b32f758e6cebd7b5181b3b8e022161
ts560hz <56908908+ts560hz@users.noreply.github.com>
2023-11-28 22:59:10 +0100

    finished exercise 12 and all exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  const oldest = array.sort(function (a, b) {
+    let lastGuy;
+    let newGuy;
+    let date = new Date();
+    let thisYear = date.getFullYear();
+    if (!a.yearOfDeath) {
+      lastGuy = thisYear - a.yearOfBirth;
+    } else {
+      lastGuy = a.yearOfDeath - a.yearOfBirth;
+    }
+    if (!b.yearOfDeath) {
+      newGuy = thisYear - b.yearOfBirth;
+    } else {
+      newGuy = b.yearOfDeath - b.yearOfBirth;
+    }
+    return lastGuy > newGuy ? -1 : 1;
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line
