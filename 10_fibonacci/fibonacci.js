a0ec3d8c72f47e0b0d78c6627c2608e8ae66dd42
ts560hz <56908908+ts560hz@users.noreply.github.com>
2023-11-28 22:26:06 +0100

    finished exercises 11 and 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  num = Number(num);
+  if (num < 0) return "OOPS";
+  if (num < 2) {
+    return num;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (source) {
+  let titles = [];
+  for (let i = 0; i < source.length; i++) {
+    titles[i] = source[i].title;
+  }
+  return titles;
 };
 
 // Do not edit below this line
